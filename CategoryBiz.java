7d2572e3c1cd333a075bdfda523335397f469779
Monlogue <andy13134@outlook.com>
2017-04-21 12:05:15 +0800

    CategoryBiz
    this is a demo

---
+++ CategoryBiz.java
@@ -0,0 +1,16 @@
+package com.pers.blog.biz;
+
+import com.pers.blog.entity.Category;
+import com.pers.common.biz.BaseBiz;
+
+/** 
+ * @Description 
+ * @TODO 这家伙很懒，什么也没留下！
+ * @author Lias 
+ * @date 创建时间：2016-12-31 下午1:36:24 
+ */
+
+public interface CategoryBiz extends BaseBiz<Category>
+{
+
+}
